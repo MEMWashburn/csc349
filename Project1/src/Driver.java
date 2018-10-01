eb53c4553b7a74ceb03ed6b3bf1697c49d218dd6
Angel de la Torre <angel.dlt.1996@gmail.com>
2018-10-01 12:57:58 -0700

    "Merge Sort, still buggy"

--- Project1/src/Driver.java
+++ Project1/src/Driver.java
@@ -1,8 +1,37 @@
 import java.lang.*;
+import java.util.Arrays;
 
 public class Driver {
-    public static void main(String[] args) {
-        
-    }
+    public static int[] listOne = {91, 55, 88, 7, 18, 66, 72, 62, 77, 52, 96, 56, 42, 4,
+                      68, 23, 13, 78, 22, 60, 94, 89, 98, 80, 74, 47, 81, 
+                      17, 65, 41, 45, 30, 8, 12, 83, 3, 9, 19, 90, 64, 87, 
+                      25, 46, 97, 6, 5, 61, 38, 40, 53, 43, 92, 16, 29, 28, 
+                      39, 86, 67, 34, 31, 82, 84, 99, 48, 36, 70, 73, 24, 
+                      75, 27, 85, 2, 14, 37, 95, 10, 58, 20, 35, 76, 71, 79, 
+                      1, 44, 69, 21, 54, 11, 93, 51, 33, 59, 63, 57, 15, 26, 
+                      49, 32, 50, 100};
 
+    public static int[] sortedListOne = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 
+                           15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 
+                           27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 
+                           39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 
+                           51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 
+                           63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 
+                           75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 
+                           87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 
+                           99, 100};
+    
+    public static void main(String[] args) {
+      Sorts algSorts = new Sorts();
+      int[] temp = listOne;
+
+      algSorts.selectionSort(temp,100);
+      System.out.println(Arrays.toString(temp));
+      System.out.println("List one got sorted: " + Arrays.equals(temp,sortedListOne));
+    
+      temp = listOne;
+      algSorts.mergeSort(temp,100);
+      System.out.println(Arrays.toString(temp));
+      System.out.println("List one got sorted: " + Arrays.equals(temp, sortedListOne));
+    }
 }
--- Project1/src/Sorts.java
+++ Project1/src/Sorts.java
@@ -1,6 +1,7 @@
-/*
+/* CSC 349-03
+ * Angel de la Torre & Megan Washburn
  *
- *
+ * Project 1
  */
 
 import java.lang.*;
@@ -11,8 +12,9 @@ public class Sorts {
     public static void selectionSort(int[] arr, int N) {
         int temp;
         int minIndex = 0;
-        for (int i = 0; i <= N; i++) {
-            for (int j = i; j <= N; j++) {
+        for (int i = 0; i < N; i++) {
+            minIndex = i; 
+            for (int j = i+1; j < N; j++) {
                 if (arr[j] < arr[minIndex]) {
                     minIndex = j;
                 }
@@ -25,7 +27,56 @@ public class Sorts {
     }
 
     public static void mergeSort(int[] arr, int N) {
+      mergeSort(arr,0,N-1);
+    }
 
+    private static void mergeSort(int[] arr,int first, int last){
+      if (first < last){
+         int middle = (first +last)/2;
+         mergeSort(arr,first,middle);
+         mergeSort(arr,middle+1, last);
+         mergeSortHalves(arr,first,middle,last);
+      }
+    }
+
+    private static void mergeSortHalves(int[] arr,int left, int middle, int right){
+       int temp[] = new int[right-left+1];
+       int index1 = middle - left +1;
+       int index2 = middle + 1;
+       int index = 0;
+
+       System.out.println("Size of temp array: " + temp.length);
+       while (index1 !=  middle && index2 !=  right) {
+            if ( arr[index1] <= arr[index2] ){
+               temp[index] = arr[index1];
+               index1++;
+            }
+            else {
+               temp[index] = arr[index2];
+               index2++;
+            }
+            index++;
+       }
+       System.out.println("Middle is: " + middle);
+
+      
+       while ( index1 <= middle ){
+          System.out.println("Index 1 is: " + index1 + "\nIndex is: " + index);
+          temp[index] = arr[index1];
+          index1++;
+          index++;
+       }
+        System.out.println("Right is: " + right);
+       while ( index2 < right){
+          System.out.println("Index 2 is: " + index2 + "\nIndex is: " + index);
+          temp[index] = arr[index2];
+          index2++;
+          index++;
+       }
+       
+       for(int i = left; i < right; i++){
+         arr[i] = temp[i];
+       }
     }
 
     public static void quickSort(int[] arr, int N) {
