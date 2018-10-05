b0737b21db08a01af56954cbb593d361f949c5b5
Megan Washburn <memwashburn@gmail.com>
2018-10-05 03:17:51 -0700

    Minor edits.

--- Project1/src/SortTimes.java
+++ Project1/src/SortTimes.java
@@ -5,47 +5,45 @@ public class SortTimes {
 
    public static void main(String[] args){
       int[] og = new int[160000];
-      int[] copy1 = new int[160000];
-      int[] copy2 = new int[160000];
-      int[] copy3 = new int[160000];
+      int[] copy1, copy2, copy3;
 
-      long startTime,endTime,nanoTime;
-      long selectTime,mergeTime,quickTime;
+      long startTime, endTime, nanoTime;
+      long selectTime, mergeTime, quickTime;
 
       Sorts algSorts = new Sorts();
       Random rand = new Random();
 
-      for(int i = 0; i < 160000;i++){
+      for(int i = 0; i < 160000; i++){
          og[i] = rand.nextInt();
       }
 
       copy1 = copy2 = copy3 = og;
       
       System.out.println("Running Times of three sorting algorithms:\n");
-      for(int n = 5000; n <= 160000; n*=2){
-         for(int j = 0; j < 5;j++){
+      for(int n = 5000; n <= 160000; n*=2) {
+         for(int j = 0; j < 5; j++) {
             startTime = System.nanoTime();
-            algSorts.selectionSort(copy1,n);
+            algSorts.selectionSort(copy1, n);
             endTime = System.nanoTime();
 
             nanoTime = endTime - startTime;
-            selectTime = nanoTime/1000000;
+            selectTime = nanoTime / 1000000;
 
             startTime = System.nanoTime();
-            algSorts.mergeSort(copy1,n);
+            algSorts.mergeSort(copy2, n);
             endTime = System.nanoTime();
 
             nanoTime = endTime - startTime;
-            mergeTime = nanoTime/1000000;
+            mergeTime = nanoTime / 1000000;
 
             startTime = System.nanoTime();
-            algSorts.quickSort(copy1,n);
+            algSorts.quickSort(copy3, n);
             endTime = System.nanoTime();
 
             nanoTime = endTime - startTime;
-            quickTime = nanoTime/1000000;
+            quickTime = nanoTime / 1000000;
 
-            System.out.println("N="+ n + ": T_ss=" + selectTime + ", T_ms=" + mergeTime + ", T_qs=" + quickTime);
+            System.out.println("N=" + n + ":\t T_ss=" + selectTime + ",\t T_ms=" + mergeTime + ",\t T_qs=" + quickTime);
          }
          System.out.println();
      }
