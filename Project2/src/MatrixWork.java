25ac7337dfad6f9f8fe5f69625443de2b57fdea1
Megan Washburn <memwashburn@gmail.com>
2018-10-08 12:44:57 -0700

    Basic fileIO for main()

--- Project2/src/MatrixWork.java
+++ Project2/src/MatrixWork.java
@@ -4,17 +4,41 @@
 
 import java.io.*;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MatrixWork {
-    private static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+
         System.out.println("Enter the input file name, A: ");
         String fileNameA = reader.nextLine();
-
-        System.out.println("Enter the input file name, B: ");
-        String fileNameB = reader.nextLine();
-
         reader.close();
+
+        File file = new File(fileNameA);
+        BufferedReader bf = null;
+        List<Integer> list = new ArrayList<Integer>();
+
+        try {
+            bf = new BufferedReader(new FileReader(file));
+            String text = null;
+
+            while ((text = bf.readLine()) != null) {
+                list.add(Integer.parseInt(text));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bf != null) {
+                    bf.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private static int[][] matrixProduct(int[][]A, int[][]B)
