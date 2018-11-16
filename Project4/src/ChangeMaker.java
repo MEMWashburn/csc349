098e1cfdfd6e86b8ac88f4b739de4faaac322d2d
Angel de la Torre <angel.dlt.1996@gmail.com>
2018-11-15 19:47:27 -0800

    Finished all 3 parts, need to revist greedy algorithm as in the tests, DP and Greedy are not different

--- Project4/src/ChangeMaker.java
+++ Project4/src/ChangeMaker.java
@@ -11,61 +11,104 @@ public class ChangeMaker {
     public static void main(String args[]) {
         Scanner reader = new Scanner(System.in);
 
-        System.out.print("Enter the number of coins: ");
-        int k = reader.nextInt();
+        System.out.print("Enter the number of coin-denominations and " + 
+            "the set of coin values: ");
         
-        System.out.print("Enter the values for k coins in decreasing order: ");
+        int k = reader.nextInt();        
         int[] d = new int[k];
 
         for(int i = 0; i < k;i++){
            d[i] = reader.nextInt();
         }
-        System.out.println(Arrays.toString(d));
-        
-        System.out.print("Enter the change you would like ( 0 to quit): ");
-        
-        int n;
 
-        if( (n = reader.nextInt()) == 0){
-            return;
-        }
+        //System.out.println(Arrays.toString(d));
+        
+        int n = 1;
+
+        while(true){
+            System.out.print("Enter a positive amount to be changed (0 to quit): ");
+            if( (n = reader.nextInt()) == 0)
+               break;
        
-        int[] result = change_DP(n, d);
-        printResults(result,d,n);
+            int[] result = change_DP(n, d);
+            System.out.println("DP algorithm results");
+            printResults(result,d,n);
+            
+            result = change_greedy(n,d);
+            System.out.println("Greedy algorithm results");
+            printResults(result,d,n);
+
+        }
+
+        System.out.println("Thanks for playing. Good bye.");
     }
       
     public static int[] change_DP(int n, int[] d) {
-        int j = n;
-        int k = d.length;
+        // C will hold the number of coins needed
+        int[] C = new int[n+1];
+        // A will hold the minimum index for a coin used
+        int[] A = new int[n+1];
+        // B will hold the frequency of each coin used for the change
+        int[] B = new int[d.length];
+         
+        C[0] = 0;
+        A[0] = 0;
 
-        int[] C = new int[n];
-        int[] A = new int[j];
-        int[] B = new int[k];
+        int min = d.length;
+        int index = 0; 
+        for(int j=1; j <= n;j++){
+           min = d.length;
+           for(int i = 0; i < d.length;i++){
+              if( (j-d[i]) >= 0){
+                 if(min > i){
+                    min = i;
+                    index = j-d[i];
+                    A[j] = i;
+                 }
+              }
+           }
 
-        C[0] = 0;     
+           C[j] = 1 + C[index];
+       }
+       //System.out.println(Arrays.toString(C));
+       //System.out.println(Arrays.toString(A));
 
-        for (int i = 1; i < n; i++) {
-            if (j >= d[i]) {
-               C[j] = C[j - d[i]];
+       while( n > 0){
+          index = A[n];
+          B[index] += 1;
+          n -= d[index];
+       }
+       //System.out.println(Arrays.toString(B));
+       return B;
+    }
+    
+    public static int[] change_greedy(int n, int[] d){
+         int[] B = new int[d.length];
+         
+         int i = 0;
+         while(n >= 0 && i < d.length){
+            if( (n-d[i]) >= 0){
+               B[i] += 1;
+               n = n - d[i];
             }
-                return B;
+            else {
+               i++;
             }
-        }
-        return B;
+         }
+         return B;
     }
 
-    private static void printResults(result,d,n){
-       System.out.println("DP algorithm results");
-       System.out.println("Amount" + n);
+    private static void printResults(int[] result,int[] d,int n){
+       System.out.println("Amount: " + n);
 
        System.out.print("Optimal distribution: ");
-       int first = 1;
+       boolean first = true;
        int coinCount = 0; 
        for(int i = 0; i < d.length; i++){
           if(result[i] != 0){
              if(first){
                 System.out.print(result[i] + "*" + d[i] + "c ");
-                frist = 0;
+                first = false;
              }
              else{
                 System.out.print("+ " + result[i] + "*" + d[i] + "c ");
@@ -73,7 +116,7 @@ public class ChangeMaker {
              coinCount += result[i];
           }
        }
-       System.out.println("\nOptimal coin count: " + coinCount);
+       System.out.println("\nOptimal coin count: " + coinCount + "\n");
     }
                
 }
---
+++ Project4/src/Tester.java
@@ -0,0 +1,73 @@
+/**
+ * CSC 349-03 Project 4
+ * 16 November 2018
+ * Angel de la Torre    ardelato@calpoly.edu
+ * Megan Washburn       mwashbur@calpoly.edu
+ */
+
+import java.util.*;
+
+public class Tester {
+    public static void main(String args[]) {
+         
+         int[] set1 = {100,50,25,10,5,1};
+         int[] set2 = {100,50,20,15,10,5,3,2,1};
+         int[] set3 = {64,32,16,8,4,2,1};
+         int[] set4 = {100,50,25,10,1};
+         int[] set5 = {66,25,27,18,10,1};
+         
+         int[] dpResult;
+         int[] greedyResult;
+
+         int n = 200;
+         
+         int set1Matches;
+         int set2Matches;
+         int set3Matches;
+         int set4Matches;
+         int set5Matches; 
+
+         set1Matches = set2Matches = set3Matches = set4Matches = set5Matches = 0; 
+
+         for(int j = 1; j <= n; j++){
+            //Set 1
+            dpResult = ChangeMaker.change_DP(j,set1);
+            greedyResult = ChangeMaker.change_greedy(j,set1);
+            if(Arrays.equals(dpResult,greedyResult))
+               set1Matches++;
+            
+            //Set 2
+            dpResult = ChangeMaker.change_DP(j,set2);
+            greedyResult = ChangeMaker.change_greedy(j,set2);
+            if(Arrays.equals(dpResult,greedyResult))
+               set2Matches++;
+            
+            //Set 3
+            dpResult = ChangeMaker.change_DP(j,set3);
+            greedyResult = ChangeMaker.change_greedy(j,set3);
+            if(Arrays.equals(dpResult,greedyResult))
+               set3Matches++;
+            
+            //Set 4
+            dpResult = ChangeMaker.change_DP(j,set4);
+            greedyResult = ChangeMaker.change_greedy(j,set4);
+            if(Arrays.equals(dpResult,greedyResult))
+               set4Matches++;
+            
+            //Set 5
+            dpResult = ChangeMaker.change_DP(j,set5);
+            greedyResult = ChangeMaker.change_greedy(j,set5);
+            if(Arrays.equals(dpResult,greedyResult))
+               set5Matches++;
+         
+         }
+
+         System.out.println("Testing change_DP and change_greedy algorithms");
+         System.out.println("Testing set1: " + set1Matches + " matches in 200 tests");
+         System.out.println("Testing set2: " + set2Matches + " matches in 200 tests");
+         System.out.println("Testing set3: " + set3Matches + " matches in 200 tests");
+         System.out.println("Testing set4: " + set4Matches + " matches in 200 tests");
+         System.out.println("Testing set5: " + set5Matches + " matches in 200 tests");
+    }
+      
+}
