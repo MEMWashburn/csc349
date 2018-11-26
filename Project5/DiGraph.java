5c4b8df2f71016c5ff24796ec1464b3c3fc0a417
Angel de la Torre <angel.dlt.1996@gmail.com>
2018-11-26 15:27:17 -0800

    Finished Part 1, need to do some more testing until moving to Part 2

--- Project5/DiGraph.java
+++ Project5/DiGraph.java
@@ -5,34 +5,93 @@
  * Megan Wasburn mwashbur@calpoly.edu
  */
 
-import java.util.*
+import java.util.*;
+import java.lang.Integer;
 
 public class DiGraph {
-   private LinkedList<Integer>[] arr; 
+   static LinkedList<Integer>[] arr; 
 
    public DiGraph(int N){
       arr = (LinkedList<Integer>[]) new LinkedList[N];
+
+      //Initialize array with empty lists
+      for(int i = 0; i < N; i++)
+         arr[i] = new LinkedList<Integer>();
+ 
    }
 
    // (from) and (to) identify vertices representing the edge that needs to added
    // to the graph.
-   // (to) vertix added as from vertex's neighbor
+   // (to) vertix added as (from) vertex's neighbor
    //
    // Notes: 
    //    The edge should not be added if it already exists, need to check before add
    //    Vertix-numbers are given in natural numbering (starting with 1) so you
    //    should "turn" them to Java-Indexing to reflext correct connection.
-   public static void addEdge(int from, int to){
-      if( !arr[from - 1].contains(Integer(to))){
-         arr[from - 1 ].add(Integer(to));
+   public static boolean addEdge(int from, int to){
+      Integer newTo = new Integer(to);
+      if( !arr[from - 1].contains(newTo)){
+         arr[from - 1 ].add(newTo);
+         return true;
       }
+      return false;
    }
-
-   public static void deleteEdge(int from, int to){
-      if( arr[from - 1].contains(Integer(to))){
-         arr[from - 1].remove(Integer(to));
-      }
-   }
-
-
    
+   // (to) vertix is removed as (from) vertex's neighbor
+   //
+   // Notes:
+   //    Nothing is done if the edge doesn't exist
+   //    Need to turn the ints from natural numbering into Java-Indexing
+   public static boolean deleteEdge(int from, int to){
+      Integer newTo = new Integer(to);
+      if( arr[from - 1].contains(newTo)){
+         arr[from - 1].remove(newTo);
+         return true;
+      }
+      return false;
+   }
+
+   // Computes and returns the number of edges in the graph
+   //
+   // Need to check for duplicates
+   public int edgeCount(){
+      int numEdges = 0;
+      
+      for(int i = 0; i < arr.length; i++){
+         numEdges += arr[i].size();
+      }
+      return numEdges;
+   }
+
+   // Returns the number of vertices in the graph
+   public int vertexCount(){
+      return arr.length;
+   }
+
+   // Outputs the graph such that:
+   //    For each vertex i (from 0 to N-1) outputs a line:
+   //       i is connected to: x1, ..., xk (where x1, ..., xk are vertices that
+   //          are adjacent to i)
+   // Note:
+   //    Numbering of vertices in the output should be natural, starting with 1
+   //    Separate two neighbors with a comma (no comma after the last neighbor)
+   public void print(){
+      int alen = arr.length;
+
+      for( int i = 0; i < alen; i++){
+         // May have to do it as .toArray(new Integer[arr[alen].size()]) 
+         LinkedList<Integer> edges = arr[i];
+
+         System.out.print( (i+1) + " is connected to: ");
+         for( int j = 0; j < edges.size(); j++){
+            if( j == 0)
+               System.out.print(edges.get(j));
+            else {
+               System.out.print(", " + edges.get(j));
+            }
+         }
+         System.out.println();
+      }
+      System.out.println();
+   }
+}   
