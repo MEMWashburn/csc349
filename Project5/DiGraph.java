ecf442e0516484fc619e8a5973196e3c3276aabc
Megan Washburn <memwashburn@gmail.com>
2018-12-03 03:10:25 -0800

    Adding scaffolding for Part III

--- Project5/DiGraph.java
+++ Project5/DiGraph.java
@@ -9,164 +9,205 @@ import java.util.*;
 import java.lang.*;
 
 public class DiGraph {
-   static LinkedList<Integer>[] arr; 
 
-   public DiGraph(int N){
-      arr = (LinkedList<Integer>[]) new LinkedList[N];
+    static LinkedList<Integer>[] arr;
 
-      //Initialize array with empty lists
-      for(int i = 0; i < N; i++)
-         arr[i] = new LinkedList<Integer>();
- 
-   }
+    /**
+     * Part III and IV
+     */
+    private class VertexInfo() {
+        int distance;     // length of the path
+        int predecessor;  // parent of a vertex
+    }
 
-   // (from) and (to) identify vertices representing the edge that needs to added
-   // to the graph.
-   // (to) vertix added as (from) vertex's neighbor
-   //
-   // Notes: 
-   //    The edge should not be added if it already exists, need to check before add
-   //    Vertix-numbers are given in natural numbering (starting with 1) so you
-   //    should "turn" them to Java-Indexing to reflext correct connection.
-   // Returns a boolean as a way to provide feedback to the user
-   public static boolean addEdge(int from, int to){
-      Integer newTo = new Integer(to);
-      if( !arr[from - 1].contains(newTo)){
-         arr[from - 1 ].add(newTo);
-         return true;
-      }
-      return false;
-   }
-   
-   // (to) vertix is removed as (from) vertex's neighbor
-   //
-   // Notes:
-   //    Nothing is done if the edge doesn't exist
-   //    Need to turn the ints from natural numbering into Java-Indexing
-   // Returns a boolean as a way to provide feedback to the user
-   public static boolean deleteEdge(int from, int to){
-      Integer newTo = new Integer(to);
-      if( arr[from - 1].contains(newTo)){
-         arr[from - 1].remove(newTo);
-         return true;
-      }
-      return false;
-   }
+    // Returns an array of VertexInfo type objects containing data that can be used
+    // to construct shortest paths from s vertex to all verties in the graph that are
+    // reachable from vertex s. NOTE: private so we can decide how the s vertex is given
+    // Use addLast and removeFirst for our regular (non-priority) queue of LinkedList objects
+    private VertexInfo[] BFS(int s) {
+        VertexInfo[] vi = new VertexInfo()[];
+        return vi;
+    }
 
-   // Computes and returns the number of edges in the graph
-   //
-   // Need to check for duplicates
-   public int edgeCount(){
-      int numEdges = 0;
-      
-      for(int i = 0; i < arr.length; i++){
-         numEdges += (arr[i].size());
-      }
-      return numEdges;
-   }
+    // Returns true if there is a path from vertex from to vertex to, false otherwise
+    public bool isTherePath(int from, int to) {
+        return false;
+    }
 
-   // Returns the number of vertices in the graph
-   public int vertexCount(){
-      return arr.length;
-   }
+    // Returnns integer, shortest distance of the to vertex from the from vertex
+    public int lengthOfPath(int from, int to) {
+        return 0;
+    }
 
-   // Outputs the graph such that:
-   //    For each vertex i (from 0 to N-1) outputs a line:
-   //       i is connected to: x1, ..., xk (where x1, ..., xk are vertices that
-   //          are adjacent to i)
-   // Note:
-   //    Numbering of vertices in the output should be natural, starting with 1
-   //    Separate two neighbors with a comma (no comma after the last neighbor)
-   public void print(){
-      int alen = arr.length;
+    // Arranges the output of the shortest distance of the to vertext from the from vertex
+    public void printPath(int from, int to) {
+        if (isTherePath(from, to)) {
+            VertexInfo[] vi = BFS(from);
+            System.out.println("VertexInfo: . . .");
+        }
+        else System.out.print("There is no path");
+    }
 
-      for( int i = 0; i < alen; i++){
-         // May have to do it as .toArray(new Integer[arr[alen].size()]) 
-         LinkedList<Integer> edges = arr[i];
 
-         System.out.print("   " + (i+1) + " is connected to: ");
-         for( int j = 0; j < edges.size(); j++){
-            if( j == 0)
-               System.out.print(edges.get(j));
-            else {
-               System.out.print(", " + edges.get(j));
+    /**
+     * Part I and II
+     */
+    public DiGraph(int N){
+        arr = (LinkedList<Integer>[]) new LinkedList[N];
+
+        //Initialize array with empty lists
+        for(int i = 0; i < N; i++)
+            arr[i] = new LinkedList<Integer>();
+
+    }
+
+    // (from) and (to) identify vertices representing the edge that needs to added
+    // to the graph.
+    // (to) vertix added as (from) vertex's neighbor
+    //
+    // Notes:
+    //    The edge should not be added if it already exists, need to check before add
+    //    Vertix-numbers are given in natural numbering (starting with 1) so you
+    //    should "turn" them to Java-Indexing to reflext correct connection.
+    // Returns a boolean as a way to provide feedback to the user
+    public static boolean addEdge(int from, int to){
+        Integer newTo = new Integer(to);
+        if( !arr[from - 1].contains(newTo)){
+            arr[from - 1 ].add(newTo);
+            return true;
+        }
+        return false;
+    }
+
+    // (to) vertex is removed as (from) vertex's neighbor
+    //
+    // Notes:
+    //    Nothing is done if the edge doesn't exist
+    //    Need to turn the ints from natural numbering into Java-Indexing
+    // Returns a boolean as a way to provide feedback to the user
+    public boolean deleteEdge(int from, int to){
+        Integer newTo = new Integer(to);
+        if( arr[from - 1].contains(newTo)){
+            arr[from - 1].remove(newTo);
+            return true;
+        }
+        return false;
+    }
+
+    // Computes and returns the number of edges in the graph
+    //
+    // Need to check for duplicates
+    public int edgeCount(){
+        int numEdges = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            numEdges += (arr[i].size());
+        }
+        return numEdges;
+    }
+
+    // Returns the number of vertices in the graph
+    public int vertexCount(){
+        return arr.length;
+    }
+
+    // Outputs the graph such that:
+    //    For each vertex i (from 0 to N-1) outputs a line:
+    //       i is connected to: x1, ..., xk (where x1, ..., xk are vertices that
+    //          are adjacent to i)
+    // Note:
+    //    Numbering of vertices in the output should be natural, starting with 1
+    //    Separate two neighbors with a comma (no comma after the last neighbor)
+    public void print(){
+        int alen = arr.length;
+
+        for( int i = 0; i < alen; i++){
+            // May have to do it as .toArray(new Integer[arr[alen].size()])
+            LinkedList<Integer> edges = arr[i];
+
+            System.out.print("   " + (i+1) + " is connected to: ");
+            for( int j = 0; j < edges.size(); j++){
+                if( j == 0)
+                    System.out.print(edges.get(j));
+                else {
+                    System.out.print(", " + edges.get(j));
+                }
             }
-         }
-         System.out.println();
-      }
-      System.out.println();
-   }
+            System.out.println();
+        }
+        System.out.println();
+    }
 
-   // Returns an array of integers representing the indegrees of all vertices in
-   // the graph:
-   //    The i-th integer in the resulting array is the indegree of the i-th
-   //    vertex 
-   private Integer[] indegrees(){
-      Integer[] inD = new Integer[arr.length];
-      
-      Arrays.fill(inD,0);
-      for(int i = 0; i < arr.length; i++){
-         for(int j = 0; j < arr[i].size(); j++){
-            Integer indexValue = arr[i].get(j);
-            inD[indexValue - 1]++;
-         }
-      }
+    // Returns an array of integers representing the indegrees of all vertices in
+    // the graph:
+    //    The i-th integer in the resulting array is the indegree of the i-th
+    //    vertex
+    private Integer[] indegrees(){
+        Integer[] inD = new Integer[arr.length];
 
-      // Prints out the array of indegrees
-      //for(int i = 0; i < inD.length; i++){
-      //   System.out.print(inD[i] + " ");
-      //}
-      //System.out.println();
-      
+        Arrays.fill(inD,0);
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].size(); j++){
+                Integer indexValue = arr[i].get(j);
+                inD[indexValue - 1]++;
+            }
+        }
 
-      return inD;
-   }
+        // Prints out the array of indegrees
+        //for(int i = 0; i < inD.length; i++){
+        //   System.out.print(inD[i] + " ");
+        //}
+        //System.out.println();
 
-   // Returns an array containing the list of topologically sorted vertices
-   //    (values in the array should represent natural vertex-numbers)
-   // Note:
-   //    If the graph is cyclic, throw an IllegalArgumentException exception
-   public Integer[] topSort() throws IllegalArgumentException {
-      Integer[] in = indegrees();
-      
-      // Will hold the sorted vertices
-      Integer[] sortedV = new Integer[arr.length];
 
-      LinkedList<Integer> q = new LinkedList<Integer>();
-      
-      int i;
-      // Check the indegree array for all the vertices with 0 indegrees
-      for(i = 0; i < arr.length; i++){
-         if( in[i] == 0)
-            q.add(i+1);
-      }
-      
-      // Check if the graph is cyclic
-      //    i.e. all vertices have an indegree and so nothing was added to the
-      //    queue
-      if( q.size() == 0)
-         throw new IllegalArgumentException("Cyclic Graph");
+        return inD;
+    }
 
-      i = 0;
-      
-      while(q.size() != 0){
-         
-         // System.out.println(q);
+    // Returns an array containing the list of topologically sorted vertices
+    //    (values in the array should represent natural vertex-numbers)
+    // Note:
+    //    If the graph is cyclic, throw an IllegalArgumentException exception
+    public Integer[] topSort() throws IllegalArgumentException {
+        Integer[] in = indegrees();
 
-         // q is in java indexing
-         Integer index = q.remove();
-         // sortedV holds the values in a natural Index
-         sortedV[i] = index;
-         i++;
-         
-         // Decrease the indegrees
-         for(int j = 0; j < arr[index-1].size(); j++){
-            Integer v = arr[index-1].get(j);
-            in[v-1]--;
-            if(in[v-1] == 0)
-               q.add(v);
-         }
-      }
-      return sortedV;
-   }  
+        // Will hold the sorted vertices
+        Integer[] sortedV = new Integer[arr.length];
+
+        LinkedList<Integer> q = new LinkedList<Integer>();
+
+        int i;
+        // Check the indegree array for all the vertices with 0 indegrees
+        for(i = 0; i < arr.length; i++){
+            if( in[i] == 0)
+                q.add(i+1);
+        }
+
+        // Check if the graph is cyclic
+        //    i.e. all vertices have an indegree and so nothing was added to the
+        //    queue
+        if( q.size() == 0)
+            throw new IllegalArgumentException("Cyclic Graph");
+
+        i = 0;
+
+        while(q.size() != 0){
+
+            // System.out.println(q);
+
+            // q is in java indexing
+            Integer index = q.remove();
+            // sortedV holds the values in a natural Index
+            sortedV[i] = index;
+            i++;
+
+            // Decrease the indegrees
+            for(int j = 0; j < arr[index-1].size(); j++){
+                Integer v = arr[index-1].get(j);
+                in[v-1]--;
+                if(in[v-1] == 0)
+                    q.add(v);
+            }
+        }
+        return sortedV;
+    }
 }   
--- Project5/DiGraphTest.java
+++ Project5/DiGraphTest.java
@@ -8,134 +8,155 @@
 import java.util.*;
 
 public class DiGraphTest {
-  
-   public static void main(String[] args){
-      Scanner reader = new Scanner(System.in);
-      
-      // Prompt user for number of vertices
-      System.out.print("Enter the number of vertices: ");
-      DiGraph graph = new DiGraph(reader.nextInt());
-      
-      // Print menu for the user (only print once)
-      // As long as the user does not choose to quit, do the following:
-      //    * Analyze the choice and execute via a switch statement
-      //       * Default case will print "invalid menu choice"
-      //    * Prompt the user for the appropriate parameters in addEdge & 
-      //       deleteEdge
-      //    * For every request, output feedback
-      //       * Added/Deleted - "Edge (x,y) was added/deleted"
-      //       * Edge/Vertex Count - "Number of edges | vertices is: "
-      //       * Print Graph - "The graph is the following: "
-      //
-      // Note:
-      //    In all communications with the user, the vertex numbering is
-      //    natural, starting at 1
-      
-      String menu = "Choose one of the following operations:\n"
-         + "   -add edge (enter a)\n"
-         + "   -delete edge (enter d)\n"
-         + "   -edge count (enter e)\n"
-         + "   -vertex count (enter v)\n"
-         + "   -print graph (enter p)\n"
-         + "   -topological sort (enter t)\n"
-         + "   -Quit (enter q)\n";
-               
-      System.out.print(menu + "\n> ");
 
-      // Read a line
-      String option = reader.nextLine();
-      char o = ' '; 
+    public static void main(String[] args){
+        /**
+         * Part III and VI
+         * i, l, s
+         */
 
-      while(o != 'q') { 
-         if (option.length() == 0){
-            while(reader.hasNext() && option.length() == 0)
-               option = reader.nextLine();
-         }
-        
-         if(option.length() > 2)
-            System.out.println("Invalid menu choice");
-         
-         else{
-            o = option.charAt(0);
-            switch (o){
-               case 'a':
-                  helperAdd(graph, reader);
-                  break;
-               case 'd':
-                  helperDelete(graph, reader);
-                  break;
-               case 'e':
-                  System.out.println("Number of edges is: " + graph.edgeCount() + "\n");
-                  break;
-               case 'v':
-                  System.out.println("Number of vertices is: " + graph.vertexCount() + "\n");
-                  break;
-               case 'p':
-                  System.out.println("The graph is the following: ");
-                  graph.print();
-                  break;
-               case 't':
-                  helperTSort(graph);
-                  break;
-               case 'q':
-                  break;
-               default:
-                  System.out.println("Invalid menu choice");
-            }  
-         }
-         System.out.print("> "); 
-         if(reader.hasNext())
-            option = reader.nextLine();
-      }
-      System.out.println("Good bye");
-   }
+        /**
+         * Part I and II
+         * a, d, t
+         */
+        Scanner reader = new Scanner(System.in);
 
-   // Helper Method to carry out the functionality for the add command
-   private static void helperAdd(DiGraph graph,Scanner reader){
-      System.out.print("Enter the 'from' and 'to' vertices of the edge to add: ");
-      
-      int from = reader.nextInt();
-      int to = reader.nextInt();
-      
-      if(graph.addEdge(from,to))
-         System.out.println("Edge (" + from + ", " + to + ") was added\n");
-      else {
-         System.out.println("Edge has already been added to the graph\n");
-      }
-   }
+        // Prompt user for number of vertices
+        System.out.print("Enter the number of vertices: ");
+        DiGraph graph = new DiGraph(reader.nextInt());
 
-   // Helper Method to carry out the functionality for the delete command
-   private static void helperDelete(DiGraph graph,Scanner reader){
-      System.out.print("Enter the 'from' and 'to' vertices of the edge to delete: ");
-      
-      int from = reader.nextInt();
-      int to = reader.nextInt();
-      
-      if(graph.deleteEdge(from,to))
-         System.out.println("Edge (" + from + ", " + to + ") was deleted\n");
-      else {
-         System.out.println("Edge does not already exist in the graph\n");
-      }
-   }
+        // Print menu for the user (only print once)
+        // As long as the user does not choose to quit, do the following:
+        //    * Analyze the choice and execute via a switch statement
+        //       * Default case will print "invalid menu choice"
+        //    * Prompt the user for the appropriate parameters in addEdge &
+        //       deleteEdge
+        //    * For every request, output feedback
+        //       * Added/Deleted - "Edge (x,y) was added/deleted"
+        //       * Edge/Vertex Count - "Number of edges | vertices is: "
+        //       * Print Graph - "The graph is the following: "
+        //
+        // Note:
+        //    In all communications with the user, the vertex numbering is
+        //    natural, starting at 1
 
-   // Helper Method to carry out the functionality for the topological sort
-   // command
-   private static void helperTSort(DiGraph graph){
-      Integer[] sortedList;
-      try {
-         sortedList = graph.topSort();
-         System.out.print("The topologically sorted sequence of vertices: ");
-         
-         for(int i = 0; i < sortedList.length; i++){
-            if( i == 0)
-               System.out.print(sortedList[i]);
-            else{
-               System.out.print(", " + sortedList[i]);
+        String menu = "Choose one of the following operations:\n"
+                + "   -add edge (enter a)\n"
+                + "   -delete edge (enter d)\n"
+                + "   -edge count (enter e)\n"
+                + "   -vertex count (enter v)\n"
+                + "   -print graph (enter p)\n"
+                + "   -topological sort (enter t)\n"
+                + "   -Quit (enter q)\n";
+
+        System.out.print(menu + "\n> ");
+
+        // Read a line
+        String option = reader.nextLine();
+        char o = ' ';
+
+        while(o != 'q') {
+            if (option.length() == 0){
+                while(reader.hasNext() && option.length() == 0)
+                    option = reader.nextLine();
             }
-         }
-         System.out.println();
-      } catch (IllegalArgumentException e) {
-         System.out.println(e);
-      }
-   }
+
+            if(option.length() > 2)
+                System.out.println("Invalid menu choice");
+
+            else{
+                o = option.charAt(0);
+                switch (o){
+                    case 'a':
+                        helperAdd(graph, reader);
+                        break;
+                    case 'd':
+                        helperDelete(graph, reader);
+                        break;
+                    case 'e':
+                        System.out.println("Number of edges is: " + graph.edgeCount() + "\n");
+                        break;
+                    case 'v':
+                        System.out.println("Number of vertices is: " + graph.vertexCount() + "\n");
+                        break;
+                    case 'p':
+                        System.out.println("The graph is the following: ");
+                        graph.print();
+                        break;
+                    case 't':
+                        helperTSort(graph);
+                        break;
+                    case 'q':
+                        break;
+                    case 'i':
+                        System.out.println("Given two vertices, find if there is a path from the first to the second.");
+                        // helperIsPath
+                        break;
+                    case 'l':
+                        System.out.println("Given two vertices, find length of the shortest path from first to second.");
+                        // helperPathLength
+                        break;
+                    case 's':
+                        System.out.println("Given two vertices, print shortest path from first to second, if exists.");
+                        // helperShortestPath
+                        break;
+                    default:
+                        System.out.println("Invalid menu choice");
+                }
+            }
+            System.out.print("> ");
+            if(reader.hasNext())
+                option = reader.nextLine();
+        }
+        System.out.println("Good bye");
+    }
+
+    // Helper Method to carry out the functionality for the add command
+    private static void helperAdd(DiGraph graph,Scanner reader){
+        System.out.print("Enter the 'from' and 'to' vertices of the edge to add: ");
+
+        int from = reader.nextInt();
+        int to = reader.nextInt();
+
+        if(graph.addEdge(from,to))
+            System.out.println("Edge (" + from + ", " + to + ") was added\n");
+        else {
+            System.out.println("Edge has already been added to the graph\n");
+        }
+    }
+
+    // Helper Method to carry out the functionality for the delete command
+    private static void helperDelete(DiGraph graph,Scanner reader){
+        System.out.print("Enter the 'from' and 'to' vertices of the edge to delete: ");
+
+        int from = reader.nextInt();
+        int to = reader.nextInt();
+
+        if(graph.deleteEdge(from,to))
+            System.out.println("Edge (" + from + ", " + to + ") was deleted\n");
+        else {
+            System.out.println("Edge does not already exist in the graph\n");
+        }
+    }
+
+    // Helper Method to carry out the functionality for the topological sort
+    // command
+    private static void helperTSort(DiGraph graph){
+        Integer[] sortedList;
+        try {
+            sortedList = graph.topSort();
+            System.out.print("The topologically sorted sequence of vertices: ");
+
+            for(int i = 0; i < sortedList.length; i++){
+                if( i == 0)
+                    System.out.print(sortedList[i]);
+                else{
+                    System.out.print(", " + sortedList[i]);
+                }
+            }
+            System.out.println();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
+    }
 }
